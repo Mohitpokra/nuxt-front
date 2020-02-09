eaa8a398a731c47792969b451800a8d5c473a335
avinashD <avinash147.1193@gmail.com>
2020-02-09 17:09:57 +0530

    margin and padding done

--- pages/exp-home/index.vue
+++ pages/exp-home/index.vue
@@ -1,26 +1,35 @@
 <template>
   <b-container>
       <b-row>
-          <b-col cols="12"  lg="11" offset-lg="1">
+          <b-col cols="12"  lg="11" offset-lg="1" class="top-title" >
             <h2>What are you looking to do today?</h2>
          </b-col>
-          <b-col class="col-container" cols="12" lg="auto" offset-lg="1">
+          <b-col class="col-container top-container" cols="12" lg="auto" offset-lg="1">
               <div class="img-container">
                   <img class="icon-option-new-search" src="~/assets/icons/icon-option-new-search.svg" />
               </div>
               <h3 class="m-text">Start a new home search</h3>
+              <div class="img-tick-mark" cols="12" lg="auto" offset-lg="1">
+                  <img class="check" src="~/assets/icons/icon-interface-check.svg" />
+              </div>
           </b-col>
           <b-col class="col-container" cols="12" lg="auto" >
               <div class="img-container">
-                  <img class="icon-option-new-search" src="~/assets/icons/icon-option-new-search.svg" />
+                  <img class="icon-option-new-search" src="~/assets/icons/icon-option-previous-search.svg" />
               </div>
               <h3 class="m-text">Start a new home search</h3>
+              <div class="img-tick-mark">
+                  <img class="check" src="~/assets/icons/icon-interface-check.svg" />
+              </div>
           </b-col>
               <b-col class="col-container" cols="12" lg="auto" >
               <div class="img-container">
-                  <img class="icon-option-new-search" src="~/assets/icons/icon-option-new-search.svg" />
+                  <img class="icon-option-new-search" src="~/assets/icons/icon-option-preapproval.svg" />
               </div>
               <h3 class="m-text">Start a new home search</h3>
+              <div class="img-tick-mark">
+                  <img class="check" src="~/assets/icons/icon-interface-check.svg" />
+              </div>
           </b-col>
       </b-row>
   </b-container>
@@ -48,6 +57,13 @@ export default {
 }
 
 .col-container {
+  transition: box-shadow .3s;
+  margin-top: 42px;
+ 
+  @media screen and (max-width: 992px) {
+        margin-top: 8px;
+    }
+  margin-right: 24px;
   width: 296px;
   height: 392px;
   border-radius: 2px;
@@ -55,9 +71,45 @@ export default {
   background-color: #ffffff;
 }
 
+.col-container:hover{
+  cursor: pointer; 
+  box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.12);
+  background-color: #ffffff;
+}
+
+.top-container{
+  @media screen and (max-width: 992px) {
+        margin-top: 48px;
+    }
+}
 .m-text {
-  margin-bottom: 80px;
-   margin-left: 48px;
+  margin-top: 24px;
+  margin-left: 48px;
   margin-right: 48px;
 }
+
+.top-title{
+  margin-top: 92px;
+  @media screen and (max-width: 992px) {
+        margin-top: 42px;
+    }
+}
+
+.check{
+  width: 16px;
+  height: 11px;
+  object-fit: contain;
+}
+
+.img-tick-mark{
+    width: 32px;
+    height: 32px;
+    margin-top: 35px;
+    margin-left: 216px;
+    @media screen and (max-width: 992px) {
+        margin-top: 16px;
+        margin-left: 287px;
+    }
+}
+
 </style>
