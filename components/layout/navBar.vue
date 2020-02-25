44b3f0d79c50bfa137606178ca69ba3ba3d3fafb
viveksinghup <viveksinghup.singh@gmail.com>
2020-02-25 23:39:47 +0530

    sidebar compoent added

--- components/layout/navBar.vue
+++ components/layout/navBar.vue
@@ -9,8 +9,21 @@
         <b-navbar-nav class="ml-auto" v-else>
             <b-nav-text class="p3">Already have an account?</b-nav-text>
             <b-nav-item class="p3 text-primary" to="/sign_in"><span class="text-primary">Sign In</span></b-nav-item>
+            <div class="sidebar-icon">
+                <img class="logo" src="~/assets/icons/icon-interface-menu.svg" />
+            </div>
         </b-navbar-nav>
     </b-navbar>
+    <div class="sidebar-menu">
+        <img class="cross" src="~/assets/icons/icon-interface-x.svg" />
+        <ul>
+            <li>Home</li>
+            <li>Start a new home search</li>
+            <li>Revisit a previous search</li>
+            <li>Request a pre-approval</li>
+            <li>My Account</li>
+        </ul>
+    </div>
 </b-container>
 </template>
 
@@ -36,6 +49,50 @@ ul {
 
     li {
         padding-bottom: 0px;
+    }
+}
+.sidebar{
+    &-icon{
+        display: flex;
+        align-items: center;
+        display: none;
+    }
+    &-menu{
+        position: fixed;
+        right: 0px;
+        transform: translateX(468px);
+        top: 0px;
+        transition: 0.45s;
+        bottom: 0px;
+        background-color: #fff;
+        padding: 30px 72px;
+        width: 468px;
+        z-index: 1;
+        box-shadow: -8px 0 24px 0 rgba(0, 0, 0, 0.08);
+        &.reveal{
+            transform: translateX(0px);
+        }
+        .cross{
+            float: right;
+            width: 35px;
+            padding: 10px;
+            height: 35px;
+            cursor: pointer;
+            margin-top: -20px;
+        }
+        ul{
+            margin-top: 50px;
+            padding: 0px;
+            li{
+                list-style: none;
+                font-size: 18px;
+                font-weight: 600;
+                line-height: 1.33;
+                color: #44a1bf;
+                margin-bottom: 24px;
+                cursor: pointer;
+            }
+        }
     }
 }
 
