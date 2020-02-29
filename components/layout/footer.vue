59fd98ae1fc83f47d25f4b99a8bd31fc09622d63
Vivek Singh <viveksinghup.singh@gmail.com>
2020-02-29 13:32:46 +0530

    globle footer spacing added for mobile

--- components/layout/footer.vue
+++ components/layout/footer.vue
@@ -5,7 +5,7 @@
             <b-col cols="12" md="4">
                 <p class="mobile-center mb-0 p4">© 2019 Search By Mortgage Payment</p>
             </b-col>
-            <b-col cols="12" md="4" class="">
+            <b-col cols="12" md="4" class="social-link">
                 <div class="d-flex justify-content-center">
                     <b-link href="#" class="img_container"><img class="fb" src="~/assets/icons/icon-social-facebook.svg" /></b-link>
                     <b-link href="#" class="img_container_1"><img class="insta" src="~/assets/icons/icon-social-instagram.svg" /></b-link>
@@ -67,5 +67,10 @@ export default {
 
 .m-left-text {
     margin-left: 24px;
+}
+@media screen and (max-width: 720px) {
+    .social-link{
+        margin: 15px 0px;
+    }
 }
 </style>
