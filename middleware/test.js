e1a49095cb405be170562eb3eccecf3b76c5d9d1
Naman Bansal <naman.bansal@shakedeal.com>
2020-03-27 00:27:24 +0530

    updated route fo resetpassword

--- middleware/test.js
+++ middleware/test.js
@@ -9,4 +9,14 @@ export default function ({
   if (loggedIn && (['sign_in','sign_up','sign_in-forgot_password','resetpassword'].indexOf(routeName) > -1)) {
     redirect('/exp-home')
   }
+  
+  
+  
+  if (!loggedIn && (['resetpassword'].indexOf(routeName) > -1)) {
+	const query = route.query;
+	const token = query && query.token;
+	if(!token){
+		redirect('/')
+	}
+  }
 }
